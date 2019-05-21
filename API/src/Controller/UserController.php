b605a38ffe952347c062d3167d44e18fd5991e78
yannisoo <yannis.b8@gmail.com>
2019-05-21 15:40:43 +0200

    21/05 15:40 Ajout user table + api

---
+++ API/src/Controller/UserController.php
@@ -0,0 +1,102 @@
+<?php
+namespace App\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use App\Entity\User;
+use App\Form\UserType;
+/**
+ * Movie controller.
+ * @Route("/api", name="api_")
+ */
+class UserController extends FOSRestController
+{
+    /**
+     * Lists all Pages.
+     * @Rest\Get("/users")
+     *
+     * @return Response
+     */
+    public function getUserAction()
+    {
+        $repository = $this->getDoctrine()->getRepository(User::class);
+        $pages = $repository->findall();
+        return $this->handleView($this->view($pages));
+    }
+
+    /**
+     * List one Page.
+     * @Rest\Get("/users/{id}")
+     *
+     * @param $id
+     * @return Response
+     */
+    public function getSingleUserAction($id)
+    {
+        $repository = $this->getDoctrine()->getRepository(User::class);
+        $pages = $repository->find($id);
+        return $this->handleView($this->view($pages));
+    }
+
+    /**
+     * Create Page.
+     * @Rest\Post("/user")
+     *
+     * @return Response
+     */
+    public function postUserAction(Request $request)
+    {
+        $page = new User();
+        $form = $this->createForm(UserType::class, $page);
+        $data = json_decode($request->getContent(), true);
+        $form->submit($data);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($page);
+            $em->flush();
+            return $this->handleView($this->view(['status' => 'ok'], Response::HTTP_CREATED));
+        }
+        return $this->handleView($this->view($form->getErrors()));
+    }
+
+    /**
+     * Update Page.
+     * @Rest\Put("/userUpdate/{id}")
+     *
+     * @return Response
+     */
+    public function UpdatePage(Request $request, User $page)
+    {
+        $form = $this->createForm(UserType::class, $page);
+        $data = json_decode($request->getContent(), true);
+        $form->submit($data);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($page);
+            $em->flush();
+            return $this->handleView($this->view(['status' => 'ok'], Response::HTTP_CREATED));
+        }
+        return $this->handleView($this->view($form->getErrors()));
+    }
+
+    /**
+     * Delete User.
+     * @Rest\Delete("/userDelete/{id}")
+     *
+     * @param $id
+     * @return Response
+     */
+    public function DeleteUser($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $user =  $em->getRepository(User::class)->find($id);
+
+        $em->remove($user);
+        $em->flush();
+
+        return $this->handleView($this->view(['status' => 'ok'], Response::HTTP_CREATED));
+
+    }
+}
---
+++ API/src/Form/UserType.php
@@ -0,0 +1,24 @@
+<?php
+namespace App\Form;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use App\Entity\User;
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('save', SubmitType::class)
+        ;
+    }
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => User::class,
+            'csrf_protection' => false
+        ));
+    }
+}
