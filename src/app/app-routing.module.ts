c254ebb6533a999855a029384326b228e71eceba
Quentin Flé <44965475+Karonad@users.noreply.github.com>
2019-05-22 14:17:30 +0200

    Begin FacturePage
    Modif: app-route
    créer dossier project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProjectSingleComponent } from './project/project-single/project-single.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'project-single', component: ProjectSingleComponent},
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { MainComponent } from './main/main.component';
+import { MainComponent } from './home/main/main.component';
 import { ListeComponent } from './liste-devis/liste/liste.component';
+import { ProjectNavbarComponent } from './project/project-navbar/project-navbar.component';
+import { ProjectSingleComponent } from './project/project-single/project-single.component';
 
 
 @NgModule({
@@ -13,7 +15,9 @@ import { ListeComponent } from './liste-devis/liste/liste.component';
     AppComponent,
     HeaderComponent,
     MainComponent,
-    ListeComponent
+    ListeComponent,
+    ProjectNavbarComponent,
+    ProjectSingleComponent
   ],
   imports: [
     BrowserModule,
