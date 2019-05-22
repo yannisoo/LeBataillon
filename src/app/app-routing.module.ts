0a533f1ef38f4a4abd96a3a16c9b555689690325
yannisoo <yannis.b8@gmail.com>
2019-05-22 16:39:49 +0200

    22/05 16:39 Merge completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { FormComponent } from './create-project/form/form.component';
 import { ProjectSingleComponent } from './project/project-single/project-single.component';
+import { MainComponent } from './home/main/main.component';
 
 
 const routes: Routes = [
   { path: 'project-single', component: ProjectSingleComponent},
-   { path: 'create-project', component: FormComponent}
+  { path: 'create-project', component: FormComponent},
+  { path: '', component: MainComponent}
 ];
 
 
