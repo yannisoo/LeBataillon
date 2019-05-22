0d92d03de4a9360e55c480b38b8b8a036d51d982
Luka Michelson <luka.michelson@gmail.com>
2019-05-22 10:11:13 +0200

    22/05

---
+++ src/app/liste-devis/liste/liste.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-liste',
+  templateUrl: './liste.component.html',
+  styleUrls: ['./liste.component.sass']
+})
+export class ListeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
