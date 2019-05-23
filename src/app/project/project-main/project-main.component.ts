5f8602428f355228ce3c40a7ee22eac77fb36ff6
Quentin Flé <44965475+Karonad@users.noreply.github.com>
2019-05-23 15:33:35 +0200

    Facture page
    23/05 - 15:30
    Project-main

---
+++ src/app/project/project-main/project-main.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-project-main',
+  templateUrl: './project-main.component.html',
+  styleUrls: ['./project-main.component.sass']
+})
+export class ProjectMainComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
