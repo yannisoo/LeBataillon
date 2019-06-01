7ea8bf4acaa245ac96d264706e1d5429f85952a0
yannisoo <yannis.b8@gmail.com>
2019-06-01 05:22:43 +0200

    01/06 05:22 ajout zippage + mail service juridique

--- API/src/Controller/QuotationController.php
+++ API/src/Controller/QuotationController.php
@@ -7,6 +7,7 @@ use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use App\Entity\Quotation;
+use App\Entity\Bill;
 use App\Entity\Agency;
 use App\Entity\Project;
 use App\Form\QuotationType;
@@ -172,6 +173,79 @@ class QuotationController extends FOSRestController
         return $this->handleView($this->view(['status' => 'ok'], Response::HTTP_CREATED));
 
     }
+
+
+    /**
+     * justique Quotation.
+     * @Rest\Post("/zipzip/{id}")
+     *
+     *@param $id
+     * @return Response
+     */
+    public function JuridiqueQuotation(Request $request, \Swift_Mailer $mailer, $id)
+    {
+
+
+            $repositoryProject = $this->getDoctrine()->getRepository(Project::class);
+            $project = $repositoryProject->find($id);
+
+            $repositoryBill = $this->getDoctrine()->getRepository(Bill::class);
+            $bill = $repositoryBill->findBy([ 'project_id' => $id]);
+
+            $repositoryQuotation = $this->getDoctrine()->getRepository(Quotation::class);
+            $quotation = $repositoryQuotation->findBy([ 'project_id' => $id]);
+
+
+            // Create new Zip Archive.
+            $zip = new \ZipArchive();
+
+            // The name of the Zip documents.
+            $zipName = $project->getName() .  '.zip';
+
+
+
+            $zip->open($zipName,  \ZipArchive::CREATE);
+            foreach ($bill as $bill) {
+              $zip->addFromString(basename($bill->getPdfPath()),  file_get_contents('.' .  $bill->getPdfPath()));
+            }
+            foreach ($quotation as $quotation) {
+              $zip->addFromString(basename($bill->getPdfPath()),  file_get_contents('.' .  $bill->getPdfPath()));
+            }
+            $zip->close();
+
+
+            $response = new Response(file_get_contents($zipName));
+            $response->headers->set('Content-Type', 'application/zip');
+            $response->headers->set('Content-Disposition', 'attachment;filename="' . $zipName . '"');
+            $response->headers->set('Content-length', filesize($zipName));
+
+
+
+            $message = (new \Swift_Message('Hello Email'))
+                ->setFrom('angsymftest@gmail.com')
+                ->setTo('yannis.b8@gmail.com')
+                ->attach(Swift_Attachment::fromPath($zipName))
+                ->setBody(
+                    $this->renderView(
+                        'emails/facture_email.html.twig', [
+                            'name' => $project->getName(),
+                            'descritpion' => $project->getDescription()
+
+                    ]),
+                    'text/html'
+                );
+
+            $mailer->send($message);
+            return $this->handleView($this->view(['status' => 'ok']));
+        }
+
+
+
+
+
+
+
+
     /**
      * Send Quotation.
      * @Rest\Post("/quotationSend/{id}")
